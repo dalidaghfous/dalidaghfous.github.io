db146d076f327b565f09033ab020c570821771a6
Mohamed-Ali Daghfous <med.ali.daghfous@codencia.com>
2021-04-24 02:14:49 +0100

    Updates

---
+++ precache-manifest.ac5ecfbd3cf8ea00bb7065cf8476b760.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421d4346bdb6f499625628b3b418065b",
+    "url": "/dalidaghfous.github.io/index.html"
+  },
+  {
+    "revision": "a4e38786855b7271b71d",
+    "url": "/dalidaghfous.github.io/static/css/2.0e6937d3.chunk.css"
+  },
+  {
+    "revision": "4bc821c200288bd87c3e",
+    "url": "/dalidaghfous.github.io/static/css/main.324ed8e2.chunk.css"
+  },
+  {
+    "revision": "a4e38786855b7271b71d",
+    "url": "/dalidaghfous.github.io/static/js/2.169dfd62.chunk.js"
+  },
+  {
+    "revision": "4bc821c200288bd87c3e",
+    "url": "/dalidaghfous.github.io/static/js/main.da79c9b2.chunk.js"
+  },
+  {
+    "revision": "ff403892923c52704758",
+    "url": "/dalidaghfous.github.io/static/js/runtime-main.dd818e8d.js"
+  },
+  {
+    "revision": "5c653daace009c6d0d9c4fd8f7f265ff",
+    "url": "/dalidaghfous.github.io/static/media/cv.5c653daa.png"
+  },
+  {
+    "revision": "54c8b12a4400ad41b9c408c170cc2070",
+    "url": "/dalidaghfous.github.io/static/media/cv1.54c8b12a.png"
+  },
+  {
+    "revision": "4c1cba5f7496cf94240aa8aa3d67f76a",
+    "url": "/dalidaghfous.github.io/static/media/cv2.4c1cba5f.png"
+  },
+  {
+    "revision": "2da239b51f31c877b33bb3bf4e81c849",
+    "url": "/dalidaghfous.github.io/static/media/cv3.2da239b5.png"
+  },
+  {
+    "revision": "fdf35f0af93d30d092cb284563dbb732",
+    "url": "/dalidaghfous.github.io/static/media/cv4.fdf35f0a.png"
+  },
+  {
+    "revision": "648e0d09821a99ecb204499aeaff1a6e",
+    "url": "/dalidaghfous.github.io/static/media/dalyxos_grey_512.648e0d09.png"
+  },
+  {
+    "revision": "c71b23920826dbc8cdd792370a7efb68",
+    "url": "/dalidaghfous.github.io/static/media/dx_logo.c71b2392.png"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/dalidaghfous.github.io/static/media/git.c31b5069.svg"
+  }
+]);
